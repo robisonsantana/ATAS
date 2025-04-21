c70772a2b6f1359a09f8f1fa7e0543ecad3c6b8d
brenda <dollanganger.8@gmail.com>
2025-04-21 20:13:39 -0300

    feat: added starter page

--- src/main/java/com/sistema/tcc/projeto/home/HomeController.java
+++ src/main/java/com/sistema/tcc/projeto/home/HomeController.java
@@ -1,12 +1,14 @@
 package com.sistema.tcc.projeto.home;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home() {
         return "home/home";
     }
