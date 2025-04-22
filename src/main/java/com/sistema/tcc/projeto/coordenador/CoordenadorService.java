ae12946f2e1f672d77242f4eb1a52365601b4c94
brenda <dollanganger.8@gmail.com>
2025-04-22 00:13:36 -0300

    feat: added coordenador

---
+++ src/main/java/com/sistema/tcc/projeto/coordenador/CoordenadorService.java
@@ -0,0 +1,11 @@
+package com.sistema.tcc.projeto.coordenador;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoordenadorService {
+
+    public void SolicitarEdital () throws Exception{
+
+    }
+}
