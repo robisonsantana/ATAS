e0da28af94a406249f8c2985b82b59ea10693264
Anthony Oliveira <thony.sox@gmail.com>
2025-04-29 16:31:52 -0300

    Criação da entidade Ata

---
+++ src/main/java/com/sistema/tcc/projeto/ata/Ata.java
@@ -0,0 +1,9 @@
+package com.sistema.tcc.projeto.ata;
+
+import com.sistema.tcc.projeto.usuario.Usuario;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Ata extends Usuario {
+
+}
---
+++ src/main/java/com/sistema/tcc/projeto/ata/AtaService.java
@@ -0,0 +1,8 @@
+package com.sistema.tcc.projeto.ata;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AtaService {
+
+}
