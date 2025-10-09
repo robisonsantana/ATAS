68678f4f525b12a674674b4884a8e0a04d548ede
brenda <dollanganger.8@gmail.com>
2025-10-09 20:20:36 -0300

    fix: deleção de pastas antigas de java (inuteis) e adicão da estrutura backend da disciplina, não deve ser realizado o merge após esse commit, pois possui erros na conexão (leitura incorreta dos dados do formulário) pt2

--- atasystem/src/app/app.ts
+++ atasystem/src/app/app.ts
@@ -3,21 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
 import { SubHeaderComponent } from './components/subheader/subheader';
 import { FooterComponent } from './components/footer/footer';
-import {TestService} from './test.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, SubHeaderComponent, FooterComponent],
-  providers: [TestService],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
+export class App  {
   protected readonly title = signal('atasystem');
 
-  private testService = inject(TestService);
-
-  ngOnInit(): void {
-    this.testService.testBackend();
-  }
 }
