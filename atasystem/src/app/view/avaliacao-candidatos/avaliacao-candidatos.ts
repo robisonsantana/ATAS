d65ee582f3f308af78fb9934a72e42d18174bf96
Celline Bitencourt <cellinebitencourt@gmail.com>
2025-10-27 22:22:19 -0300

    fix: arrumando tela de avaliação de candidatos

--- atasystem/src/app/view/avaliacao-candidatos/avaliacao-candidatos.ts
+++ atasystem/src/app/view/avaliacao-candidatos/avaliacao-candidatos.ts
@@ -10,24 +10,28 @@ import { CommonModule } from '@angular/common';
   templateUrl: './avaliacao-candidatos.html',
   styleUrl: './avaliacao-candidatos.css',
 })
+
 export class AvaliacaoCandidatos {
+  editalSelecionado: Edital = new Edital(
+    1,
+    'Edital 001/2024 - Engenharia de Software',
+    '001/2024',
+    [
+      new Candidato('Marcos', '12345678904', 'MG1234571', 'Motivo Teste32', 28, ['doc7.pdf', 'doc10.pdf', 'doc11.pdf']),
+      new Candidato('Beatriz', '12345678905', 'MG1234572', 'Motivo Teste33', 29, ['doc8.pdf', 'doc12.pdf', 'doc13.pdf']),
+      new Candidato('Renan', '12345678906', 'MG1234573', 'Motivo Teste34', 27, ['doc9.pdf', 'doc14.pdf', 'doc15.pdf']),
+    ],
+    'Engenharia de Software',
+    'Aberto',
+    new Date('2024-05-01'),
+    'Presencial',
+    2025
+  );
 
-  editalSelecionado?: Edital;
+  candidatoSelecionado?: Candidato;
 
-  editais: Edital[] = [
-    new Edital(1, 'Edital 001/2024 - Engenharia de Software', '001/2024', [
-        new Candidato('Marcos', '12345678904', 'MG1234571', 'Motivo Teste32', 28, ['doc7.pdf']),
-        new Candidato('Beatriz', '12345678905', 'MG1234572', 'Motivo Teste33', 29, ['doc8.pdf']),
-        new Candidato('Renan', '12345678906', 'MG1234573', 'Motivo Teste34', 27, ['doc9.pdf'])], 
-        'Engenharia de Software', 'Aberto', new Date('2024-05-01'), 'Presencial', 2025),
-    new Edital(2, 'Edital 002/2025 - Banco de Dados', '002/2024', [
-        new Candidato('Ana', '12345678901', 'MG1234568', 'Motivo Teste1', 25, ['doc1.pdf', 'doc2.pdf']),
-        new Candidato('Bruno', '12345678902', 'MG1234569', 'Motivo Teste2', 27, ['doc3.pdf', 'doc4.pdf'])], 
-        'Banco de Dados', 'Em Análise', new Date('2024-06-15'), 'EAD', 2024),
-    new Edital(3, 'Edital 003/2024 - Redes de Computadores', '003/2024', [
-        new Candidato('Carla', '12345678907', 'MG1234574', 'Motivo Teste35', 26, ['doc10.pdf']),
-        new Candidato('Diego', '12345678908', 'MG1234575', 'Motivo Teste36', 24, ['doc11.pdf'])],
-        'Redes de Computadores', 'Aberto', new Date('2024-07-20'), 'Presencial', 2025),
-  ]
-
+  // Método para selecionar o candidato
+  selecionarCandidato(candidato: Candidato) {
+    this.candidatoSelecionado = candidato;
+  }
 }
